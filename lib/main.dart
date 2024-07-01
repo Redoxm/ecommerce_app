84b39028fee5974cb068ab619f0438bcdd0d468a
Luqman adebayo <luqmanaremu4@gmail.com>
2024-07-01 21:30:20 +0100

    dhd

--- lib/main.dart
+++ lib/main.dart
@@ -4,3 +4,17 @@ import 'package:t_store/app.dart';
 void main() {
   runApp(const App());
 }
+
+class name extends StatefulWidget {
+  const name({super.key});
+
+  @override
+  State<name> createState() => _nameState();
+}
+
+class _nameState extends State<name> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
